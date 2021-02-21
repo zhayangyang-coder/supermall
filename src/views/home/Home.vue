d9fbdde974805014e1b1a6f91dbc6530f4e019b3
zhayangyang <1571516409@qq.com>
2021-02-21 21:43:02 +0800

    引入并使用navbar组件和axios请求数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar>
+      <div slot="center" class="home-nav-bar">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+  // 导入navbar
+  import NavBar from "@/components/common/navbar/NavBar";
+  // 导入api
+  import { getHomeMultidata } from "network/home/home"
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(result => {
+        this.banners = result.data.banner;
+        this.recommends = result.data.recommend;
+      })
+    },
   }
 </script>
 
 <style scoped>
-
+  .home-nav-bar {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
